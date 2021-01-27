a8d85715ff79042f7344f613d5709a2bac337879
Melissa Lawrence <mlawrence537@gmail.com>
2021-01-27 21:46:57 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,11 @@
+require 'pry'
+
+def reverse_each_word(sentence)
+  sentence_array = sentence.split(" ")
+  reverse_array = []
+  sentence_array.collect do |element|
+    reverse_array.push(element.reverse)
+  end
+  reverse_array.join(" ")
+end
+
