53fe3a53df86af31aa0502908bf5b6c081588ff3
Melissa Lawrence <mlawrence537@gmail.com>
2021-01-27 21:42:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,6 +5,7 @@ def reverse_each_word(sentence)
   sentence_array.push(sentence.split(" "))
   reverse_array = sentence_array.collect do |element|
     element.reverse
+    reverse_array.join(" ")
   end
 end
 
