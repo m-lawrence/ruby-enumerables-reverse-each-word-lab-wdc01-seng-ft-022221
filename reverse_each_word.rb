3390718cbbb8c0db46922240e67be570c8d1718e
Melissa Lawrence <mlawrence537@gmail.com>
2021-01-27 21:43:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,8 +3,9 @@ require 'pry'
 def reverse_each_word(sentence)
   sentence_array = []
   sentence_array.push(sentence.split(" "))
-  reverse_array = sentence_array.collect do |element|
-    element.reverse
+  reverse_array = []
+  sentence_array.collect do |element|
+    reverse_array.push(element.reverse)
     reverse_array.join(" ")
   end
 end
